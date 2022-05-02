4f29f7f1ae483e64cf36c8eaafa2dbd2de3332cb
Quinn Dumala <quinndumala@gmail.com>
2022-05-02 12:06:34 +0800

    config changes and jest fix

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
+    verbose: true,
     testEnvironment: "jsdom",
     moduleNameMapper: {
         ".(css|less|scss)$": "identity-obj-proxy",
-      },
+    }, 
+    allowJs: true,
+    transform: {
+        "^.+\\.(t|j)sx?$": "ts-jest",
+    },
+    transformIgnorePatterns: ["./node_modules/@codelingo/(?!react-tiny-popover/)"]
   };
